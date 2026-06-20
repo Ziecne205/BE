93ca0b2bb5e751a5904e5220f6da65456796287f
Trần Nguyễn Bảo Việt <tranjohn205@gmail.com>
2026-06-20 10:15:00 +0700

    feat(reservation): add repository query to find active booking by license plate

--- src/main/java/com/parking/repository/ReservationRepository.java
+++ src/main/java/com/parking/repository/ReservationRepository.java
@@ -11,4 +11,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     long countByVehicleType_VehicleTypeIdAndStatusInAndExpectedEntryTimeLessThanAndExpectedExitTimeGreaterThan(
             Integer vehicleTypeId, List<String> statuses, LocalDateTime end, LocalDateTime start);
+
+    List<Reservation> findByLicensePlateAndVehicleType_VehicleTypeIdAndStatusInAndExpectedExitTimeGreaterThanEqual(
+            String licensePlate, Integer vehicleTypeId, List<String> statuses, LocalDateTime time);
 }
