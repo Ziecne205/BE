38c47412f0ecedda65e9bd7dd261b7cef50d1218
Trần Nguyễn Bảo Việt <tranjohn205@gmail.com>
2026-06-22 23:16:18 +0700

    feat(staff): update Check-In DTOs for Barie operation

---
+++ src/main/java/com/parking/modules/staff/CheckInResponse.java
@@ -0,0 +1,22 @@
+package com.parking.modules.staff;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CheckInResponse {
+    private Long sessionId;
+    private String licensePlateIn;
+    private LocalDateTime entryTime;
+    private String suggestedSlotCode;
+    private boolean isReserved;
+}
