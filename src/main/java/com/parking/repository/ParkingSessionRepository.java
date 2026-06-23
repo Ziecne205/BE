28c2fc1bd5f7194f73d38a14201d2fe1fe506260
Trần Nguyễn Bảo Việt <tranjohn205@gmail.com>
2026-06-23 13:13:59 +0700

    feat(staff): add findByStatusIn query for active session listing

--- src/main/java/com/parking/repository/ParkingSessionRepository.java
+++ src/main/java/com/parking/repository/ParkingSessionRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface ParkingSessionRepository extends JpaRepository<ParkingSession, Long> {
     Optional<ParkingSession> findFirstByLicensePlateInAndStatusIn(String licensePlateIn, List<String> statuses);
+    List<ParkingSession> findByStatusIn(List<String> statuses);
     List<ParkingSession> findByDriver_UserId(Long userId);
     long countByVehicleType_VehicleTypeIdAndStatusIn(Integer vehicleTypeId, List<String> statuses);
 }
