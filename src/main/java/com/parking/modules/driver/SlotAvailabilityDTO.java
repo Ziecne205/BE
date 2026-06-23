92bf194bf846e24334c721afaeeec91389f5dc8f
Dang Thanh Phat <shiginami2510@gmail.com>
2026-06-23 13:58:20 +0700

    feat(driver): add SlotAvailabilityDTO for parking info search

---
+++ src/main/java/com/parking/modules/driver/SlotAvailabilityDTO.java
@@ -0,0 +1,16 @@
+package com.parking.modules.driver;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SlotAvailabilityDTO {
+    private String vehicleTypeName;
+    private long totalSlots;
+    private long availableSlots;
+}
