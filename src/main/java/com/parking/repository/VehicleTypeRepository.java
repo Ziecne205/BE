3870b55eeec5ce85c1203aee27bb8406d19a68d0
Huynh Huu Thang <thanght2606@gmail.com>
2026-06-23 13:46:52 +0700

    feat(manager): bo sung existsByTypeName vao VehicleTypeRepository de kiem tra trung ten loai xe

--- src/main/java/com/parking/repository/VehicleTypeRepository.java
+++ src/main/java/com/parking/repository/VehicleTypeRepository.java
@@ -4,5 +4,4 @@ import com.parking.entity.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VehicleTypeRepository extends JpaRepository<VehicleType, Integer> {
-    boolean existsByTypeName(String typeName);
 }
